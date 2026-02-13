52ffbf210577f80d12093182307180dd252c9148
pranalisakhare0727-glitch <pranalisakhare0727@gmail.com>
2026-02-14 00:17:11 +0530

    fix: remove extra space in ApiDemo import

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
 import Dashboard from './pages/Dashboard.jsx'
-import ApiDemo from "./pages/ApiDemo.jsx";  // correct!
-
+import ApiDemo from './pages/Apidemo.jsx'
 
 
 function App() {
